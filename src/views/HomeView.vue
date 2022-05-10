8c68b64d3286c741ca248095c9a322beef8e45ef
VictorGonet <64852698+VictorGonet@users.noreply.github.com>
2022-05-10 18:23:28 +0200

    ajout de bouton

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,44 @@
 <template>
   <div class="home">
-      bonjour a tojs
+    <p id="titre" class="text-center">Tu préfères ....</p>  
+    <v-spacer></v-spacer>
+    <v-row justify="space-around">
+        <v-col md="1">
+        <v-hover
+          v-slot="{ hover }"
+        >
+          <v-btn 
+              :elevation="hover ? 10 : 4"
+              :class="{ 'on-hover': hover }"
+              class="mx-auto"
+              height="350"
+              width="350" 
+              rounded
+              :style="{ 'background-color': hover ? '#E57373' : 'red' }"
+              >
+              Bouton de gauche
+          </v-btn>
+        </v-hover>
+      </v-col>
 
+      <v-col md="3">
+        <v-hover
+          v-slot="{ hover }"
+        >
+          <v-btn 
+              :elevation="hover ? 10 : 4"
+              :class="{ 'on-hover': hover }"
+              class="mx-auto"
+              height="350"
+              width="350" 
+              rounded
+              :style="{ 'background-color': hover ? '#42A5F5' : 'blue' }"
+              >
+              Bouton de droite
+          </v-btn>
+        </v-hover>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -17,3 +54,10 @@ export default {
   }
 }
 </script>
+
+<style>
+  #titre {
+    font-size: 40px;
+  }
+
+</style>
